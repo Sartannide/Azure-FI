0bcc25756413a45753dcea76cfab0f74d4c94f9d
navatte_l <navatt_l@etna-alternance.net>
2021-06-21 11:33:12 +0200

    added basic web page

---
+++ FranceInter.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Le port du masque est obligatoire, toute personne ne portant pas de masque se verra oblitere !'
